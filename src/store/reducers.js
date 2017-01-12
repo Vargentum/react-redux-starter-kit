249e5a93ab80e31d0ba2e65c4b3c5412226a7955
Vargentum <vargentum@gmail.com>
2017-01-12 13:41:20 +0200

    add redux-ui and redux-crud-store reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux'
 import locationReducer from './location'
+import { crudReducer } from 'redux-crud-store'
+import { reducer as uiReducer } from 'redux-ui'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
     location: locationReducer,
+    models: crudReducer,
+    ui: uiReducer,
     ...asyncReducers
   })
 }
